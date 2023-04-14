e5882002085adae4a466e3d88d309802f526ea28
hyunwungSEO <sddsk1123@naver.com>
2023-04-14 22:18:29 +0900

    first commit

---
+++ src/MyGym.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const MyGym = () => {
+  return (
+    <div className="MyGym">
+      <div>드가자</div>
+    </div>
+  );
+}
+
+export default MyGym;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyGym from './MyGym';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MyGym />
+  </React.StrictMode>
+);
